544950c7bce99ffb89b577a64c770a1a08b350b5
youssraaa <74369761+youssraaa@users.noreply.github.com>
2025-10-26 22:56:08 +0100

    feat(board:state): ajout de usestate pour gérer la liste des tâches

--- projet-kanban/src/components/Board.jsx
+++ projet-kanban/src/components/Board.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react'
 import Column from './Column'
 
-function Board() {
-  
-  const tasks = [
+const initialTasks  = [
     { id: 1, title: 'Apprendre React', description: 'Découvrir les composants et les hooks.', status: 'todo' },
     { id: 2, title: 'Faire le projet Kanban', description: 'Mettre en place les colonnes et le routage.', status: 'doing' },
     { id: 3, title: 'Installer React', description: 'Projet initialisé avec Vite.', status: 'done' },
   ]
+
+function Board() {
+  
+  const [tasks, setTasks] = useState(initialTasks) 
 
   const todo = tasks.filter(t => t.status === 'todo')
   const doing = tasks.filter(t => t.status === 'doing')
